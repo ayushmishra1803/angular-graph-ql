ebbc5bfe74e444966f2734527c26a2b15cca4b79
Ayush Mishra <ayushmishra0810@gmail.com>
2021-08-10 21:47:50 +0530

    Chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'graphql';
+  constructor(private apollo: Apollo) {}
+  ngOnInit(): void {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "INR") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((res: any) => {
+        console.log(res.data);
+        console.log(res.loading);
+        console.log(res.error);
+      });
+  }
 }
